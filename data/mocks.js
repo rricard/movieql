3059a484e3d9123e4c409ba9d6fe52052bbe1a4e
Robin Ricard <ricard.robin@gmail.com>
2016-08-27 18:05:16 +0200

    Setup mocks system

---
+++ data/mocks.js
@@ -0,0 +1,7 @@
+/* @flow */
+
+const mocks =  {
+
+};
+
+export default mocks;
--- data/schema.js
+++ data/schema.js
@@ -1,13 +1,22 @@
 /* @flow */
 
-import { makeExecutableSchema } from 'graphql-tools';
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+} from 'graphql-tools';
 
 import typeDefs from './definitions';
 import resolvers from './resolvers';
+import mocks from './mocks';
+
+const isMocked = process.env.MOCKED === 'true';
+
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
+
+isMocked && addMockFunctionsToSchema({schema, mocks});
 
 export default schema;
