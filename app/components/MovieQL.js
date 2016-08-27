b03177033807ae1a1d8e292b35887f128951d0ab
Robin Ricard <ricard.robin@gmail.com>
2016-08-27 23:49:29 +0200

    Common booting for iOS/Android

---
+++ app/components/MovieQL.js
@@ -0,0 +1,41 @@
+/* @flow */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+type Platform = 'IOS'|'ANDROID';
+
+type MovieQLProps = {
+  platform: Platform,
+};
+
+const MovieQL = (props: MovieQLProps): ?React.Element<*> => {
+  const {} = props;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        TODO
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default MovieQL;
---
+++ app/index.js
@@ -0,0 +1,9 @@
+/* @flow*/
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+import MovieQL from './components/MovieQL';
+
+AppRegistry.registerComponent('movieql', () => MovieQL);
