5361744f0efb862724f4b3aa0838848728c6f4f5
Robin Ricard <ricard.robin@gmail.com>
2016-08-28 15:59:17 +0200

    Scaffold the MovieRow element

--- app/components/MovieRow.js
+++ app/components/MovieRow.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {
+  StyleSheet,
+  View,
   Text,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
 import gql from 'graphql-tag';
 import {connect} from 'react-redux';
@@ -10,17 +14,71 @@ import {
 
 import {createFragment} from 'apollo-client';
 
+type CharacterInMovie = {
+  id: string,
+  actor: {
+    id: string,
+    name: string,
+    profileUrl: string,
+  },
+};
+
 export type SmallMovieInfo = {
   id: string,
-  title: string
+  title: string,
+  tagline: ?string,
+  characters: Array<CharacterInMovie>,
 };
 
 export const smallMovieInfoFragment = createFragment(gql`
   fragment SmallMovieInfo on Movie {
     id
     title
+    tagline
+    posterUrl
+    characters {
+      id
+      actor {
+        id
+        name
+        pictureUrl
+      }
+    }
   }
 `);
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  poster: {
+    width: 100,
+    height: 150,
+    backgroundColor: '#ccc',
+  },
+  info: {
+    flexDirection: 'column',
+    margin: 10,
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+  },
+  tagline: {
+    fontSize: 14,
+    color: '#222',
+    flex: 1,
+  },
+  bestActors: {
+    margin: 10,
+  },
+  actor: {
+
+  },
+});
 
 type MovieRowProps = {
   movie: SmallMovieInfo,
@@ -30,7 +88,27 @@ type MovieRowProps = {
 const MovieRow = (props: MovieRowProps): ?React.Element<*> => {
   const {movie, openMovie} = props;
   return (
-    <Text onPress={openMovie}>{movie.title}</Text>
+    <TouchableOpacity onPress={openMovie}>
+      <View style={styles.container}>
+        <Image
+          style={styles.poster}
+          source={{
+            uri: movie.posterUrl,
+          }}
+        />
+        <View style={styles.info}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.tagline}>{movie.tagline}</Text>
+          <View style={styles.bestActors}>
+            {movie.characters.slice(0, 2).map(({id, actor}) =>
+              <View key={id} style={styles.actor}>
+                <Text>{actor.name}</Text>
+              </View>
+            )}
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
