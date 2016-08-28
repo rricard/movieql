8309a0f0865ed31911862fae72c8908860ff9e62
Pierre-Marie Dartus <p.dartus@salesforce.com>
2016-08-28 18:26:23 +0200

    Update actor list view

--- app/components/ActorRow.js
+++ app/components/ActorRow.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {
   Text,
+  TouchableOpacity,
+  StyleSheet,
+  View,
 } from 'react-native';
 import gql from 'graphql-tag';
 import {connect} from 'react-redux';
@@ -9,6 +12,8 @@ import {
 } from 'react-native-router-redux';
 
 import {createFragment} from 'apollo-client';
+
+import JpegImage from './ui/JpegImage';
 
 export type SmallActorInfo = {
   id: string,
@@ -19,8 +24,42 @@ export const smallActorInfoFragment = createFragment(gql`
   fragment SmallActorInfo on Actor {
     id
     name
+    pictureUrl
+    characters {
+      id
+      movie {
+        title
+      }
+    }
   }
 `);
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  actorPicture: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ccc',
+  },
+  info: {
+    flexDirection: 'column',
+    margin: 10,
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  movie: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+});
 
 type ActorRowProps = {
   actor: SmallActorInfo,
@@ -30,7 +69,24 @@ type ActorRowProps = {
 const ActorRow = (props: ActorRowProps): ?React.Element<*> => {
   const {actor, openActor} = props;
   return (
-    <Text onPress={openActor}>{actor.name}</Text>
+    <TouchableOpacity onPress={openActor}>
+      <View style={styles.container}>
+        <JpegImage
+          style={styles.actorPicture}
+          uri={actor.pictureUrl}
+        />
+        <View style={styles.info}>
+          <Text style={styles.title}>{actor.name}</Text>
+          <View>
+            {actor.characters.slice(0, 2).map(({id, movie}) =>
+              <View key={id} style={styles.movie}>
+                <Text>{movie.title}</Text>
+              </View>
+            )}
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
